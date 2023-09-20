f0da63d6ca4a127fbae6277c1c35bfab6cee520d
Michael Saxon <xmichaelsaxon@gmail.com>
2023-09-19 17:01:18 -0700

    debug for init run

--- cococrola/generate/models/huggingface_diffusers.py
+++ cococrola/generate/models/huggingface_diffusers.py
@@ -12,9 +12,10 @@ from cococrola.generate.models.image_generator import ImageGenerator
 class DiffusersImageGenerator(ImageGenerator):
     def __init__(self, model_id, device, pipeline_type : Type[DiffusionPipeline] = StableDiffusionPipeline) -> None:
         self.pipe = pipeline_type.from_pretrained(model_id)
+        self.device = device
         self.pipe.to(device)
 
     def generate(self, prompt: str, guidance_scale : float = 7.5, num_img: int = 9) -> List[Image.Image]:
-        with autocast(self.pipe.device):
+        with autocast(self.device):
             images += self.pipe(prompt, guidance_scale = guidance_scale, num_images_per_prompt = num_img).images
         return images
