28038fd86f779cac86e93453b7913598900c3b09
Michael Saxon <xmichaelsaxon@gmail.com>
2023-09-25 18:14:54 -0700

    added language choice option

--- cococrola/utils/simple_csv.py
+++ cococrola/utils/simple_csv.py
@@ -1,1 +1,11 @@
-# todo
+# the first line of the csv is a header containing column labels (eg, language codes)
+# return an iterator over pairs of (index value, cell contents, current line no, line[0])
+def csv_to_index_elem_iterator(input_csv_path : str, start_line : int = 1, ref_lang : str = "en"):
+    csv_lines = open(input_csv_path, "r").readlines()
+    index = csv_lines[0].strip().split(",")
+    ref_lang_idx = index.index(ref_lang)
+    for line_idx in range(start_line, len(csv_lines)):
+        line = csv_lines[line_idx]
+        line = line.strip().split(",")
+        for lang_idx, lang in enumerate(index):
+            yield (line_idx - 1, line[ref_lang_idx], lang, line[lang_idx])
--- scripts/generate_images_to_dir_start_en.py
+++ scripts/generate_images_to_dir_start_en.py
@@ -4,8 +4,9 @@ import json
 import click
 
 from cococrola.generate import models
-from cococrola.utils.click_config import CommandWithConfigFile
-
+#from cococrola.utils.click_config import CommandWithConfigFile
+from cococrola.utils.simple_csv import csv_to_index_elem_iterator
+from cococrola.utils.save_imgs import save_imgs_to_dir
 
 #@click.command(cls=CommandWithConfigFile('../config/generate.yaml'))
 @click.command()
@@ -17,35 +18,35 @@ from cococrola.utils.click_config import CommandWithConfigFile
 @click.option('--start_line', type=int, default=1)
 @click.option('--device', type=str, default="cuda")
 @click.option('--global_seed_fudge', type=int, default=0)
-@click.option('--switch_off_english_step', type=int, default=25)
-def main(model, output_dir, num_img, input_csv, prompts_base, start_line, device, global_seed_fudge, switch_off_english_step):
+@click.option('--switch_lang_step', type=int, default=25)
+@click.option('--start_lang', type=str, default="en")
+def main(model, output_dir, num_img, input_csv, prompts_base, start_line, device, global_seed_fudge, switch_lang_step, start_lang):
     generator = models.get_generator(model, device)
 
     os.makedirs(output_dir, exist_ok=True)
 
-    # put this into cococrola.utils.simple_csv
-    words = open(input_csv, "r").readlines()
-    index = words[0].strip().split(",")
     lang_prompt_templates = json.load(open(prompts_base, "r"))
 
-    # this could be put into a new generate_save_images function
-    # inputs prompts_base, lang_prompt_templates, generator, output_dir, start_line
-    for line_idx in range(start_line, len(words)):
-        line = words[line_idx]
-        line_no = line_idx - 1
-        line = line.strip().split(",")
-        english_prompt = lang_prompt_templates["en"].replace("$$$", line[0])
-        for lang_idx in range(len(index)):
-            # build a prompt based on the language-specific prompt templates
-            prompt = lang_prompt_templates[index[lang_idx]].replace("$$$", line[lang_idx])
-            print(f"generating {index[lang_idx]}:{line[0]}, '{line[lang_idx]}'")
-            # fix the concept-level seed based on the csv line we're on (same starting seed for each lang)
-            generator.update_noise_generator(seed = line_no + global_seed_fudge)
-            images = generator.generate_prompt_change(english_prompt, prompt, prompt_reset_step = switch_off_english_step, num_img = num_img)
-            for i, im in enumerate(images):
-                fname = f"{line_no}-{index[lang_idx]}-{line[0]}-{i}.png"
-                print(f"saving image {fname}...")
-                im.save(f"{output_dir}/{fname}")
+
+
+    # putting this in a different file would be overabstracting!
+    for concept_number, concept_reflang, lang_code, concept_lang in csv_to_index_elem_iterator(input_csv, start_line, ref_lang="en"):
+        # 1. build a prompt based on the language-specific prompt templates
+        prompt = lang_prompt_templates[lang_code].replace("$$$", concept_lang)
+
+        # additionally, build an english prompt specifically to use as init prompt
+        english_prompt = lang_prompt_templates[start_lang].replace("$$$", concept_reflang)
+
+        # 2. do whatever state modification we want to do to the generator
+        # fix the concept-level seed based on the csv line we're on (same starting seed for each lang)
+        generator.update_noise_generator(seed = concept_number + global_seed_fudge)
+
+        # 3. generate the images
+        images = generator.generate_prompt_change(english_prompt, prompt, prompt_reset_step = switch_lang_step, num_img = num_img)
+ 
+        # 4. save_the images
+        fname_base = f"{output_dir}/{concept_number}-{lang_code}-{concept_reflang}"
+        save_imgs_to_dir(images, fname_base)
 
 
 if __name__ == "__main__":
