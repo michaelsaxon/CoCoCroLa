71f8beb818134c52bbcb8f97bc419bc3b02112fd
Michael Saxon <xmichaelsaxon@gmail.com>
2023-10-09 14:16:04 -0700

    what is going on

--- scripts/generate_launch_html.py
+++ scripts/generate_launch_html.py
@@ -6,6 +6,7 @@ import click
 
 from cococrola.utils.build_html import build_squares
 
+
 class cd:
     """Context manager for changing the current working directory"""
     def __init__(self, newPath):
@@ -17,6 +18,15 @@ class cd:
 
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
+
+
+class NoCacheHTTPRequestHandler(
+    server.SimpleHTTPRequestHandler
+):
+    def send_response_only(self, code, message=None):
+        super().send_response_only(code, message)
+        self.send_header('Cache-Control', 'no-store, must-revalidate')
+        self.send_header('Expires', '0')
 
 #@click.command(cls=CommandWithConfigFile('../config/generate.yaml'))
 @click.command()
@@ -35,7 +45,7 @@ def main(input_csv, images_dir, base_language, socket):
         print("Saving index.html")
         open("index.html", "w").write(page_html_lines)
         print("Launching web server!")
-        with socketserver.TCPServer(("", socket), server.SimpleHTTPRequestHandler) as httpd:
+        with socketserver.TCPServer(("", socket), NoCacheHTTPRequestHandler) as httpd:
             try:
                 print("Web server running... press Ctrl+C to stop")
                 httpd.serve_forever()
