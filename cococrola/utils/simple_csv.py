e9eecde00e15a267afd0d1785215796c96e7acef
Michael Saxon <xmichaelsaxon@gmail.com>
2023-11-22 23:44:10 -0700

    added correction experiment

--- cococrola/utils/simple_csv.py
+++ cococrola/utils/simple_csv.py
@@ -3,6 +3,7 @@
 def csv_to_index_elem_iterator(input_csv_path : str, start_line : int = 1, ref_lang : str = "en"):
     csv_lines = open(input_csv_path, "r").readlines()
     index = csv_lines[0].strip().split(",")
+    print(index)
     ref_lang_idx = index.index(ref_lang)
     for line_idx in range(start_line, len(csv_lines)):
         line = csv_lines[line_idx]
