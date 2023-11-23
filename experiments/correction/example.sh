3d9f12546182b1f253e5b41221ea11f7a4b047bf
Michael Saxon <xmichaelsaxon@gmail.com>
2023-11-22 23:41:42 -0700

    added correction experiment

--- experiments/correction/example.sh
+++ experiments/correction/example.sh
@@ -6,13 +6,11 @@
 
 device="${1:-0}"
 model="${2:-SD2}"
-bs="${3:-32}"
-sb="${4:-1}"
+sb="${3:-1}"
 
 cd ../../scripts
 CUDA_VISIBLE_DEVICES="$device" python generate_images_to_dir.py \
     --input_csv ../experiments/correction/concepts_zh_jp_revised.csv \
     --output_dir ../results/correction_zh_jp_revised/"$model"/ \
     --model "$model" \
-    --batch_size "$bs" \
     --split_batch "$sb"
