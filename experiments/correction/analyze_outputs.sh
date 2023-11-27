50f9fae852fd7300816165f076985e3cad13a4e3
Michael Saxon <xmichaelsaxon@gmail.com>
2023-11-26 22:29:30 -0700

    analyze outputs

--- experiments/correction/analyze_outputs.sh
+++ experiments/correction/analyze_outputs.sh
@@ -4,8 +4,7 @@
 # second argument is model name
 # SD1-4 SD2 SD2-1 AD DE2
 
-device="${1:-0}"
-model="${2:-SD2}"
+model="${1:-SD2}"
 
 cd ../../cococrola/analyze
 CUDA_VISIBLE_DEVICES="$device" python evaluate_model.py \
