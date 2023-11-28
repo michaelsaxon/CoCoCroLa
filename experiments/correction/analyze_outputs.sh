0076dec2e90ff532cf45f3a2c96abb1646f31e31
Michael Saxon <saxon@ucsb.edu>
2023-11-28 13:06:50 -0700

    updated test

--- experiments/correction/analyze_outputs.sh
+++ experiments/correction/analyze_outputs.sh
@@ -10,8 +10,8 @@ model="${2:-SD2}"
 cd ../../cococrola/analyze
 CUDA_VISIBLE_DEVICES="$device" python evaluate_model.py \
     --input_csv ../../experiments/correction/concepts_zh_jp_revised.csv \
-    --analysis_dir ../../results/correction_zh_jp_revised/"$model"/
-
+    --analysis_dir ../../results/correction_zh_jp_revised/"$model"/ \
+    --fingerprint_selection_count 25
 #    --eval_samples_file ../../experiments/correction/lines_zh_jp_revised.csv
 
 
