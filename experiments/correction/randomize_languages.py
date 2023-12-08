b06177ce0bfe7e0da01399b552d80fffe9cf81c6
Michael Saxon <saxon@ucsb.edu>
2023-12-07 17:30:59 -0700

    randomization script

--- experiments/correction/randomize_languages.py
+++ experiments/correction/randomize_languages.py
@@ -6,10 +6,9 @@
 import click
 import random
 import json
+from tqdm import tqdm
 
 from sentence_transformers import SentenceTransformer, util
-import torch
-
 
 # takes in a sentence sim model and two sents, returns the simscores 
 def get_sim_scores_string(text_model, sent_english, sent_ml_before, sent_ml_after):
@@ -31,13 +30,15 @@ def main(input_folder):
     for lang_idx in range(1, len(index)):
         lang = index[lang_idx]
         out_lines = [f"en,{lang}-before,{lang}-after,sim-before,sim-after,sim-diff\n"]
+        print(f"Generating for {lang}...")
         template_other_lang = prompt_templates[lang]
         # for each language, generate 10 different randomized orderings
-        for _ in range(10):
+        for j in range(10):
             # generate a random ordering of the images
+            print(f"Ordering {j} of 10...")
             random_ordering = list(range(1, len(lines)))
             random.shuffle(random_ordering)
-            for i in range(1, len(lines)):
+            for i in tqdm(range(1, len(lines))):
                 true_english = lines[i].strip().split(",")[0]
                 true_other_lang = lines[i].strip().split(",")[lang_idx]
                 random_other_lang = lines[random_ordering[i-1]].strip().split(",")[lang_idx]
